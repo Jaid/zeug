0217bdcc79d6baa5d73b87f2f7e200495bf43504
Jaid <6216144+Jaid@users.noreply.github.com>
2024-02-27 03:45:48 +0100

    Improved handlebars wrappers
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/handlebars.ts
+++ src/handlebars.ts
@@ -5,16 +5,27 @@ export const makeHandlebars = () => {
   return handlebars
 }
 
-export const resolveTemplate = (template: string, context: Record<string, unknown>) => {
-  const handlebars = makeHandlebars()
-  const resolvedTemplate = handlebars.compile(template, {
+export const makeHandlebarsRenderer = (template: string) => {
+  const templateResolver = Handlebars.compile(template, {
     noEscape: true,
+    knownHelpersOnly: true,
   })
-  return resolvedTemplate(context)
+  return templateResolver
 }
 
-export const resolveHtmlTemplate = (template: string, context: Record<string, unknown>) => {
-  const handlebars = makeHandlebars()
-  const resolvedTemplate = handlebars.compile(template)
-  return resolvedTemplate(context)
+export const renderHandlebars = (template: string, context: Record<string, unknown>) => {
+  const templateResolver = makeHandlebarsRenderer(template)
+  return templateResolver(context)
+}
+
+export const makeHtmlHandlebarsRenderer = (template: string) => {
+  const templateResolver = Handlebars.compile(template, {
+    knownHelpersOnly: true,
+  })
+  return templateResolver
+}
+
+export const renderHtmlHandlebars = (template: string, context: Record<string, unknown>) => {
+  const templateResolver = makeHtmlHandlebarsRenderer(template)
+  return templateResolver(context)
 }
