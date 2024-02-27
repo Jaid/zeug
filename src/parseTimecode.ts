4b0216d75111c6db061ca4ab82649b6ffc0a954e
Jaid <6216144+Jaid@users.noreply.github.com>
2024-02-27 04:24:01 +0100

    Added parseTimecode
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

---
+++ src/parseTimecode.ts
@@ -0,0 +1,52 @@
+import {firstMatch} from 'super-regex'
+
+const regex = /^\s*((?<hours>\d+)\s*:)?\s*((?<minutes>\d+)\s*:)?\s*(?<seconds>(?<fullSeconds>\d+)(\.(?<secondsFractalDigits>\d+))?)\s*$/
+
+export const parseTimecode = (input: string) => {
+  const match = firstMatch(regex, input, {
+    timeout: 1000,
+  })?.namedGroups
+  console.log(match)
+  if (match === undefined) {
+    return
+  }
+  return match
+}
+
+export const timecodeToSeconds = (timecode: string) => {
+  const parsed = parseTimecode(timecode)
+  if (parsed === undefined) {
+    return
+  }
+  const hours = parsed.hours ? Number(parsed.hours) : 0
+  const secondsFromHours = hours * 60 * 60
+  const minutes = parsed.minutes ? Number(parsed.minutes) : 0
+  const secondsFromMinutes = minutes * 60
+  const seconds = Number(parsed.seconds)
+  return secondsFromHours + secondsFromMinutes + seconds
+}
+
+export const timecodeToSecondsString = (timecode: string) => {
+  const parsed = parseTimecode(timecode)
+  if (parsed === undefined) {
+    return
+  }
+  const hours = parsed.hours ? Number(parsed.hours) : 0
+  const secondsFromHours = hours * 60 * 60
+  const minutes = parsed.minutes ? Number(parsed.minutes) : 0
+  const secondsFromMinutes = minutes * 60
+  const seconds = Number(parsed.fullSeconds)
+  const fullSeconds = secondsFromHours + secondsFromMinutes + seconds
+  if (parsed.secondsFractalDigits === undefined) {
+    return fullSeconds.toString()
+  }
+  return `${fullSeconds}.${parsed.secondsFractalDigits}`
+}
+
+export const timecodeToMs = (timecode: string) => {
+  const seconds = timecodeToSeconds(timecode)
+  if (seconds === undefined) {
+    return
+  }
+  return Math.trunc(seconds * 1000)
+}
