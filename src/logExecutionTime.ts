5d4813be3bbb66541ac0edd782cabbfbf9291a64
Jaid <6216144+Jaid@users.noreply.github.com>
2024-03-01 05:33:34 +0100

    Renamed `WithTimer` to `logExecutionTime` and updated its JSDoc
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/logExecutionTime.ts
+++ src/logExecutionTime.ts
@@ -1,9 +1,17 @@
 type Options = {
+  /**
+   * The number of fraction digits to use when formatting the time in milliseconds.
+   * @default 2
+   */
   fractionDigits?: number
+  /**
+   * A function to call with the formatted time in milliseconds and the raw time in milliseconds.
+   * @default (msFormatted, msRaw) => console.log(`${propertyKey} took ${msFormatted} ms`)
+   */
   log?: (msFormatted: string, msRaw: number) => void
 }
 
-export function withTimer<T extends (...args: any) => any>(options: Options = {}) {
+export function logExecutionTime<T extends (...args: any) => any>(options: Options = {}) {
   return (target: any, propertyKey: string | Symbol, descriptor: PropertyDescriptor) => {
     const originalMethod = descriptor.value as T
     descriptor.value = async function (...args: Parameters<T>) {
