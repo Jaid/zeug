835021b71aaf4a793ca2706cd410756f7a295eb8
Jaid <6216144+Jaid@users.noreply.github.com>
2024-03-01 05:30:02 +0100

    Improved WithTimer
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/WithTimer.ts
+++ src/WithTimer.ts
@@ -1,18 +1,30 @@
 type Options = {
-  log?: (ms: number) => void
+  fractionDigits?: number
+  log?: (msFormatted: string, msRaw: number) => void
 }
 
-export function WithTimer<T extends (...args: any) => any>(options?: Options) {
-  return function (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
+export function withTimer<T extends (...args: any) => any>(options: Options = {}) {
+  return (target: any, propertyKey: string | Symbol, descriptor: PropertyDescriptor) => {
     const originalMethod = descriptor.value as T
     descriptor.value = async function (...args: Parameters<T>) {
       const start = performance.now()
       const result = await originalMethod.apply(this, args) as ReturnType<T>
       const end = performance.now()
-      if (options?.log) {
-        options.log(end - start)
+      const msNeeded = end - start
+      const fractionDigits = options.fractionDigits ?? 2
+      let msFormatted: string
+      if (fractionDigits === 0) {
+        msFormatted = `${Math.trunc(msNeeded)}`
+        if (msFormatted === `0`) {
+          msFormatted = `1`
+        }
       } else {
-        console.log(`${_propertyKey} took ${end - start}ms`)
+        msFormatted = msNeeded.toFixed(fractionDigits)
+      }
+      if (options.log !== undefined) {
+        options.log(msFormatted, msNeeded)
+      } else {
+        console.log(`${propertyKey} took ${msFormatted} ms`)
       }
       return result
     }
