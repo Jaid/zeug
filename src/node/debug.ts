cfe8951d1ecbaf4bbb658645dff7f62a44dee0be
Jaid <6216144+Jaid@users.noreply.github.com>
2024-04-10 05:27:01 +0200

    Removed export of default debug instance
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/node/debug.ts
+++ src/node/debug.ts
@@ -1,14 +1,12 @@
 import debug from 'debug'
 
-const mainPackageName = process.env.npm_package_name
-if (mainPackageName === undefined) {
-  throw new Error(`Cannot find package name in env var “npm_package_name”`)
-}
 export const makeDebug = (subName?: string) => {
+  const mainPackageName = process.env.npm_package_name
+  if (mainPackageName === undefined) {
+    throw new Error(`Cannot find package name in env var “npm_package_name”`)
+  }
   if (subName === undefined) {
     return debug(mainPackageName)
   }
   return debug(`${mainPackageName}:${subName}`)
 }
-const defaultDebug = makeDebug(mainPackageName)
-export {defaultDebug as debug}
