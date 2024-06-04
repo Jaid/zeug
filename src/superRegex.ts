3ad548ddfdd64216b47302c243b991f36441eaad
Jaid <6216144+Jaid@users.noreply.github.com>
2024-06-04 02:14:01 +0200

    Added more RegEx utility
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/superRegex.ts
+++ src/superRegex.ts
@@ -33,7 +33,19 @@ export const findNamedGroup = (string: string, regex: RegExp, groupName: string,
   return result?.namedGroups[groupName]
 }
 
+export const findNamedGroupGlobal = (string: string, regex: RegExp, groupName: string, matchOptions = defaultOptions) => {
+  const result = matches(regex, string, matchOptions)
+  const groupMatches = Array.from(result, match => match.namedGroups[groupName])
+  return groupMatches
+}
+
 export const findGroup = (string: string, regex: RegExp, groupIndex = 0, matchOptions = defaultSingleOptions) => {
   const result = firstMatch(regex, string, matchOptions)
   return result?.groups[groupIndex]
+}
+
+export const findGroupGlobal = (string: string, regex: RegExp, groupIndex = 0, matchOptions = defaultOptions) => {
+  const result = matches(regex, string, matchOptions)
+  const groupMatches = Array.from(result, match => match.groups[groupIndex])
+  return groupMatches
 }
