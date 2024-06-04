59eaa03013597f4f44d5b8a0fee5837871573f4d
Jaid <6216144+Jaid@users.noreply.github.com>
2024-06-04 02:08:12 +0200

    Increased default RegEx timeout
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/superRegex.ts
+++ src/superRegex.ts
@@ -10,11 +10,11 @@ export type MatchFromObject<T extends Record<string, string>> = OverrideProperti
 export type MatchFromKeys<T extends Record<string, string> | string> = MatchFromObject<Record<T extends string ? T : keyof T, string>>
 
 const defaultOptions: Parameters<typeof matches>[2] = {
-  matchTimeout: 10_000,
-  timeout: 60_000,
+  matchTimeout: 60_000,
+  timeout: 600_000,
 }
 const defaultSingleOptions: Parameters<typeof firstMatch>[2] = {
-  timeout: 60_000,
+  timeout: 600_000,
 }
 
 export const findNamedGroups = <T extends Record<string, string> | string>(string: string, regex: RegExp, matchOptions = defaultOptions) => {
